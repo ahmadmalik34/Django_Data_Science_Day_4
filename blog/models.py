e292f6b415007422152cd1d203e3b96819552af6
Muhammad Ahmad <m.ahmadmalik03@gmail.com>
2026-04-25 00:43:12 +0500

    Update blog\models.py

--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,15 @@ class Author(models.Model):
 
 class Category(models.Model):
     name=models.CharField(max_length=50,unique=True)
-    decription=models.TextField(black=true)
+    decription=models.TextField(blank=True)
 
 class Post(models.Model):
-    title=models.CharFeild(max_length=200)
+    title=models.CharField(max_length=200)
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
-    author=models.ForeignKey(Author,on_delete=models.CASCADE,related_name=posts)
-    category=models.ForeignKey(Category,related_name=posts)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,related_name='posts')
+    category=models.ForeignKey(Category,related_name='posts',on_delete=models.SET_NULL,null=True)
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Blog Post'
