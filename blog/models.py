48b7302a6fdd4728f850e1d0deb9358b0972e051
Muhammad Ahmad <m.ahmadmalik03@gmail.com>
2026-04-25 00:37:30 +0500

    Update blog\models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    bio=models.TextField(blank=True)
+
+class Category(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+    decription=models.TextField(black=true)
+
+class Post(models.Model):
+    title=models.CharFeild(max_length=200)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,related_name=posts)
+    category=models.ForeignKey(Category,related_name=posts)
